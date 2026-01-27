f0de10cda703897dd3b06f0a21ca8f012badc777
mAmineChniti <emin.chniti@esprit.tn>
2026-01-27 12:29:09 +0100

    boilerplate backend

---
+++ src/main.ts
@@ -0,0 +1,45 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+	const app = await NestFactory.create(AppModule);
+
+	app.setGlobalPrefix('api');
+
+	app.useGlobalPipes(
+		new ValidationPipe({
+			whitelist: true,
+			forbidNonWhitelisted: true,
+			transform: true,
+		}),
+	);
+
+	if (process.env.NODE_ENV !== 'production') {
+		const config = new DocumentBuilder()
+			.setTitle('Accountia API')
+			.setDescription(
+				'Business Management SaaS Platform – Multitenant System for Financial Operations and Team Collaboration',
+			)
+			.setVersion('1.0')
+			.addBearerAuth(
+				{
+					type: 'http',
+					scheme: 'bearer',
+					bearerFormat: 'JWT',
+				},
+				'access-token',
+			)
+			.build();
+
+		const document = SwaggerModule.createDocument(app, config);
+		SwaggerModule.setup('api/docs', app, document);
+	}
+
+	const port = process.env.PORT || 3000;
+	await app.listen(port);
+
+	console.log(`🚀 API running on http://localhost:${port}/api/docs`);
+}
+bootstrap();
